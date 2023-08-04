9b2d1a6f883b8955c18b1c83c561c658fbc0479c
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-08-04 23:34:49 +0200

    added basic prisma setup with user and command insert

--- src/server/controller/DefaultController.ts
+++ src/server/controller/DefaultController.ts
@@ -1,12 +1,41 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import path from 'path';
 import { handleIncomingMessages } from '../twitch/chat/handler';
+import { insertCommand, insertUser } from '../database/postgres';
 
 export const defaultRouter = Router();
 
 defaultRouter.get("/internal/v1/connect", (req: Request, res: Response) =>{
     handleIncomingMessages(req.query.user as string)
     res.send(`connected ${req.query.user}`);
+})
+
+defaultRouter.get("/internal/v1/create", async (req: Request, res: Response) =>{
+    let username: string = req.query.username as string;
+    let description: string = req.query.desc as string;
+    let isActive: boolean = true;
+    let user = await insertUser(username, isActive, description);
+    if(user != undefined){
+        res.send(`inserted User: ${user}`);
+    }else{
+        res.sendStatus(404);
+    }
+  
+})
+
+defaultRouter.get("/internal/v1/command", async (req: Request, res: Response) =>{
+    let trigger: string = req.query.trigger as string;
+    let value: string = req.query.value as string;
+    let streamerId: number = parseInt(req.query.streamerid as string)
+    let isActive: boolean = true;
+    let isRepetitive: boolean = false;
+    let command = await insertCommand(trigger, value, 0, isRepetitive, streamerId);
+    if(command != undefined){
+        res.send(`inserted User: ${command}`);
+    }else{
+        res.sendStatus(404);
+    }
+  
 })
 
 defaultRouter.get("/*", (req: Request, res: Response) => {

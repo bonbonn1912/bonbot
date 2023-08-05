1545923d876e8f17b0d1abc8ca9ca66fc88687d3
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-08-05 18:54:44 +0200

    added auth function and loading circle

---
+++ src/client/src/Functions/authUser.ts
@@ -0,0 +1,14 @@
+export const authenticateUser = () => {
+    console.log("In authenticateUser Funktion")
+  return new Promise(async (resolve, rejects) => {
+    try {
+      const response = await fetch("/authenticate");
+      const data = await response.json();
+      console.log("Username :" + data.account.display_name); 
+      resolve(data)
+    } catch (e) {
+        console.log("User ist not authenticated")
+      rejects(null);
+    }
+  });
+};
